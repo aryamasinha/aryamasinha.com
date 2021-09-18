b0fea8ec0c2fff43f9ac94ba990eaa5b688ec918
aryama <aryamasinha864@gmail.com>
2021-09-18 22:55:47 +0530

    redesign

--- README.md
+++
@@ -1,1 +0,0 @@
-Build using Next.js
--- aryamasinha/pages/index.js
+++ aryamasinha/pages/index.js
@@ -2,20 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-
 export default function Home() {
   return (
-    
-    <div>
-    <Head>
-      <title>Aryama</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <h1 className={styles.center}>
-      21.09.21
-    </h1>
-  </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Aryama</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-   
+    
+    </div>
   )
 }
